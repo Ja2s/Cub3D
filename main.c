3d3a6ba06911c73565df7b03e7f91ee74f2f5d37
jgavairo <jordan.gavairon@gmail.com>
2024-07-18 14:57:13 +0200

    little normed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jgavairo <jgavairo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 11:18:24 by jgavairo          #+#    #+#             */
-/*   Updated: 2024/07/18 11:43:52 by jgavairo         ###   ########.fr       */
+/*   Updated: 2024/07/18 14:54:50 by jgavairo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -239,130 +239,138 @@ int key_press(int keycode, t_data *data)
 	return (0);
 }
 
+void	start_and_dir(t_data *data, t_player *player)
+{
+	// Calculer la position de depart et la direction du rayon
+	data->rc.camera_x = 2 * data->rc.x / (double)data->width - 1;
+	data->rc.ray_dir_x = player->dir_x + player->plane_x * data->rc.camera_x;
+	data->rc.ray_dir_y = player->dir_y + player->plane_y * data->rc.camera_x;
+	// Dans quelle case de la carte nous sommes
+	data->rc.map_x = (int)player->pos_x;
+	data->rc.map_y = (int)player->pos_y;
+	// Longueur du rayon depuis la position actuelle jusqu'au prochain bord de case ( fabs pour passer en valeur absolue dans le cas ou nous nous orientons vers le negatif de la carte)
+	data->rc.delta_dist_x = fabs(1 / data->rc.ray_dir_x);
+	data->rc.delta_dist_y = fabs(1 / data->rc.ray_dir_y);
+}
 
+void	send_ray_helper(t_data *data, t_player *player)
+{
+	// Calculer la distance entre le joueur et la premiere ligne vertical ou horizontal que le rayon vas toucher (side dist, cest pour calculer le bord de la map ou le bord de la case ou nous sommes?) 
+	if (data->rc.ray_dir_x < 0)
+	{
+		data->rc.side_dist_x = (player->pos_x - data->rc.map_x) * data->rc.delta_dist_x;
+		data->rc.delta_dist_x = -1;
+	}
+	else
+	{
+		data->rc.side_dist_x = (data->rc.map_x + 1.0 - player->pos_x) * data->rc.delta_dist_x;
+		data->rc.delta_dist_x = 1;
+	}
+	if (data->rc.ray_dir_y < 0)
+	{
+		data->rc.side_dist_y = (player->pos_y - data->rc.map_y) * data->rc.delta_dist_y;
+		data->rc.delta_dist_y = -1;
+	}
+	else
+	{
+		data->rc.side_dist_y = (data->rc.map_y + 1.0 - player->pos_y) * data->rc.delta_dist_y;
+		data->rc.delta_dist_y = 1;
+	}
+}
+
+void	hit_checker(t_data *data)
+{
+	// Effectuer le DDA
+	data->rc.hit = 0;
+	while (data->rc.hit == 0)
+	{
+		// Passer à la prochaine case de la carte, soit en direction x, soit en direction y (on utilise fabs pour la valeur absolue, car si on avance ver une direction -- on veux savoir combien en ++)
+		if (data->rc.side_dist_x < data->rc.side_dist_y)
+		{
+			data->rc.side_dist_x += fabs(1 / data->rc.ray_dir_x);
+			data->rc.map_x += (int)data->rc.delta_dist_x;
+			data->rc.side = 0;
+		}
+		else
+		{
+			data->rc.side_dist_y += fabs(1 / data->rc.ray_dir_y);
+			data->rc.map_y += (int)data->rc.delta_dist_y;
+			data->rc.side = 1;
+		}
+		// Vérifier si le rayon a touché un mur
+		if (data->map[data->rc.map_y][data->rc.map_x] == '1')
+			data->rc.hit = 1;
+	}
+}
+
+void	col_wall_sizer(t_data *data, t_player *player)
+{
+	// calculer suivant si le mur est horizontal ou vertical le calcul qui sera necessaire au moment de dessiner le mur avec une taille differente en focntion de la distance
+	if (data->rc.side == 0)
+		data->rc.perp_wall_dist = (data->rc.map_x - player->pos_x + (1 - (int)data->rc.delta_dist_x) / 2) / data->rc.ray_dir_x;
+	else
+		data->rc.perp_wall_dist = (data->rc.map_y - player->pos_y + (1 - (int)data->rc.delta_dist_y) / 2) / data->rc.ray_dir_y;
+	// Calculer la hauteur de la ligne à dessiner sur l'écran
+	data->rc.line_height = (int)(data->height / data->rc.perp_wall_dist);
+	// Calculer les pixels les plus bas et les plus hauts pour remplir la bande actuelle
+	data->rc.draw_start = -data->rc.line_height / 2 + data->height / 2;
+	if (data->rc.draw_start < 0)
+		data->rc.draw_start = 0;
+	data->rc.draw_end = data->rc.line_height / 2 + data->height / 2;
+	if (data->rc.draw_end >= data->height)
+		data->rc.draw_end = data->height - 1;
+}
+
+void	wall_orientation(t_data *data)
+{
+	if (data->rc.side == 0 && data->rc.ray_dir_x < 0)
+		data->rc.tex_num = 0; // Mur vers l'est
+	else if (data->rc.side == 0 && data->rc.ray_dir_x >= 0)
+		data->rc.tex_num = 1; // Mur vers l'ouest
+	else if (data->rc.side == 1 && data->rc.ray_dir_y < 0)
+		data->rc.tex_num = 2; // Mur vers le nord
+	else
+		data->rc.tex_num = 3; // Mur vers le sud
+}
+
+void	send_ray(t_data *data, t_player *player)
+{
+	start_and_dir(data, player);
+	send_ray_helper(data, player);
+	hit_checker(data);
+	col_wall_sizer(data, player);
+	wall_orientation(data);
+}
 
 void raycasting(t_data *data, t_player *player)
 {
-	double camera_x;
-	double ray_dir_x;
-	double ray_dir_y;
-	int 	x;
-	int		y;
-	int		tex_num;
-	int		tex_x;
-	int		draw_end;
-	int		line_height;
-	int		draw_start;
-	double	wall_x;
-	double	perp_wall_dist;
-
-	x = 0;
+	data->rc.x = 0;
 	draw_floor_and_sky(data);
-	while (x < data->width)
+	while (data->rc.x < data->width)
 	{
-		// Calculer la position de depart et la direction du rayon
-		camera_x = 2 * x / (double)data->width - 1;
-		ray_dir_x = player->dir_x + player->plane_x * camera_x;
-		ray_dir_y = player->dir_y + player->plane_y * camera_x;
-
-		// Dans quelle case de la carte nous sommes
-		data->raycast.map_x = (int)player->pos_x;
-		data->raycast.map_y = (int)player->pos_y;
-
-		// Longueur du rayon depuis la position actuelle jusqu'à la prochaine x ou y-côté
-		data->raycast.delta_dist_x = fabs(1 / ray_dir_x);
-		data->raycast.delta_dist_y = fabs(1 / ray_dir_y);
-
-		// Calculer le pas et la distance initiale du côté
-		if (ray_dir_x < 0)
-		{
-			data->raycast.side_dist_x = (player->pos_x - data->raycast.map_x) * data->raycast.delta_dist_x;
-			data->raycast.delta_dist_x = -1;
-		}
-		else
-		{
-			data->raycast.side_dist_x = (data->raycast.map_x + 1.0 - player->pos_x) * data->raycast.delta_dist_x;
-			data->raycast.delta_dist_x = 1;
-		}
-		if (ray_dir_y < 0)
-		{
-			data->raycast.side_dist_y = (player->pos_y - data->raycast.map_y) * data->raycast.delta_dist_y;
-			data->raycast.delta_dist_y = -1;
-		}
-		else
-		{
-			data->raycast.side_dist_y = (data->raycast.map_y + 1.0 - player->pos_y) * data->raycast.delta_dist_y;
-			data->raycast.delta_dist_y = 1;
-		}
-
-		// Effectuer le DDA
-		data->raycast.hit = 0;
-		while (data->raycast.hit == 0)
-		{
-			// Passer à la prochaine case de la carte, soit en direction x, soit en direction y
-			if (data->raycast.side_dist_x < data->raycast.side_dist_y)
-			{
-				data->raycast.side_dist_x += fabs(1 / ray_dir_x);
-				data->raycast.map_x += (int)data->raycast.delta_dist_x;
-				data->raycast.side = 0;
-			}
-			else
-			{
-				data->raycast.side_dist_y += fabs(1 / ray_dir_y);
-				data->raycast.map_y += (int)data->raycast.delta_dist_y;
-				data->raycast.side = 1;
-			}
-			// Vérifier si le rayon a touché un mur
-			if (data->map[data->raycast.map_y][data->raycast.map_x] == '1')
-				data->raycast.hit = 1;
-		}
-
-		// Calculer la distance projetée sur la direction de la caméra (la distance euclidienne donnera un effet fisheye)
-		if (data->raycast.side == 0)
-			perp_wall_dist = (data->raycast.map_x - player->pos_x + (1 - (int)data->raycast.delta_dist_x) / 2) / ray_dir_x;
-		else
-			perp_wall_dist = (data->raycast.map_y - player->pos_y + (1 - (int)data->raycast.delta_dist_y) / 2) / ray_dir_y;
-
-		// Calculer la hauteur de la ligne à dessiner sur l'écran
-		line_height = (int)(data->height / perp_wall_dist);
-
-		// Calculer les pixels les plus bas et les plus hauts pour remplir la bande actuelle
-		draw_start = -line_height / 2 + data->height / 2;
-		if (draw_start < 0)
-			draw_start = 0;
-		draw_end = line_height / 2 + data->height / 2;
-		if (draw_end >= data->height)
-			draw_end = data->height - 1;
+		send_ray(data, player);
 		//calcul coordonnees textures
-		if (data->raycast.side == 0 && ray_dir_x < 0)
-			tex_num = 0; // Mur vers l'est
-		else if (data->raycast.side == 0 && ray_dir_x >= 0)
-			tex_num = 1; // Mur vers l'ouest
-		else if (data->raycast.side == 1 && ray_dir_y < 0)
-			tex_num = 2; // Mur vers le nord
+		if (data->rc.side == 0)
+			data->rc.wall_x = player->pos_y + data->rc.perp_wall_dist * data->rc.ray_dir_y;
 		else
-			tex_num = 3; // Mur vers le sud
-		if (data->raycast.side == 0)
-			wall_x = player->pos_y + perp_wall_dist * ray_dir_y;
-		else
-			wall_x = player->pos_x + perp_wall_dist * ray_dir_x;
-		wall_x -= floor(wall_x);
-		tex_x = (int)(wall_x * (double)(data->textures[tex_num].width));
-		if (data->raycast.side == 0 && ray_dir_x > 0)
-			tex_x = data->textures[tex_num].width - tex_x - 1;
-		if (data->raycast.side == 1 && ray_dir_y < 0)
-			tex_x = data->textures[tex_num].width - tex_x - 1;
+			data->rc.wall_x = player->pos_x + data->rc.perp_wall_dist * data->rc.ray_dir_x;
+		data->rc.wall_x -= floor(data->rc.wall_x);
+		data->rc.tex_x = (int)(data->rc.wall_x * (double)(data->textures[data->rc.tex_num].width));
+		if (data->rc.side == 0 && data->rc.ray_dir_x > 0)
+			data->rc.tex_x = data->textures[data->rc.tex_num].width - data->rc.tex_x - 1;
+		if (data->rc.side == 1 && data->rc.ray_dir_y < 0)
+			data->rc.tex_x = data->textures[data->rc.tex_num].width - data->rc.tex_x - 1;
 		// Dessiner les pixels de la bande en utilisant une boucle while
-		y = draw_start;
-		while (y < draw_end)
+		data->rc.y = data->rc.draw_start;
+		while (data->rc.y < data->rc.draw_end)
 		{
-			int d = y * 256 - data->height * 128 + line_height * 128;
-			int tex_y = ((d * data->textures[tex_num].height) / line_height) / 256;
-			unsigned int color = *(unsigned int*)(data->textures[tex_num].addr + (tex_y * data->textures[tex_num].line_length + tex_x * (data->textures[tex_num].bits_per_pixel / 8)));
-			my_mlx_pixel_put(data, x, y, color);
-			y++;
+			int d = data->rc.y * 256 - data->height * 128 + data->rc.line_height * 128;
+			int tex_y = ((d * data->textures[data->rc.tex_num].height) / data->rc.line_height) / 256;
+			data->rc.color = *(unsigned int*)(data->textures[data->rc.tex_num].addr + (tex_y * data->textures[data->rc.tex_num].line_length + data->rc.tex_x * (data->textures[data->rc.tex_num].bits_per_pixel / 8)));
+			my_mlx_pixel_put(data, data->rc.x, data->rc.y, data->rc.color);
+			data->rc.y++;
 		}
-		x++;
+		data->rc.x++;
 	}
 }
 
